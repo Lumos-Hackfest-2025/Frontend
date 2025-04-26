09a00aec13bd060c1a92e1d2626156dfbcf36923
Fitra Ramadhan S <fitraramadhan668@gmail.com>
2025-04-26 11:41:45 +0700

    initialize folder

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -5,17 +5,14 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 
-// Register with email and password
 export function registerWithEmail(email, password) {
   return createUserWithEmailAndPassword(auth, email, password);
 }
 
-// Login with email and password
 export function loginWithEmail(email, password) {
   return signInWithEmailAndPassword(auth, email, password);
 }
 
-// Login/Register with Google
 export function signInWithGoogle() {
   return signInWithPopup(auth, googleProvider);
 }
---
+++ src/pages/public/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      Hello
+    </div>
+  );
+}
+
+export default Home;
